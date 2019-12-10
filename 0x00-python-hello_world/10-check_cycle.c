af05343fb347034333cf6befad42f9a5256518c8
juan-skill <1294@holbertonschool.com>
2019-12-10 04:22:13 +0000

    [Fix] correction before the final change 	modified:   10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * check_cycle - checks if a singly linked list has a cycle in it
  *
@@ -16,17 +17,12 @@ int check_cycle(listint_t *head)
 
 	tmp1 = tmp2 = head;
 
-	while (tmp1 != NULL && tmp2 != NULL)
+	while (tmp1 && tmp2 && tmp2->next)
 	{
-		tmp2 = tmp2->next->next;
-		if (tmp1 == tmp2)
-			return (1);
-		if (tmp2 == NULL)
-			return (0);
-		tmp2 = tmp2->next->next;
-		if (tmp1 == tmp2)
-			return (1);
 		tmp1 = tmp1->next;
+		tmp2 = tmp2->next->next;
+		if (tmp1 == tmp2)
+			return (1);
 	}
 
 	return (0);
