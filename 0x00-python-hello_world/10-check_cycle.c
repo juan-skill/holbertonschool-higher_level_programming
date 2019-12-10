3cc36891ff2d4f0533bade1e3c2267c7c1951c0e
juan-skill <1294@holbertonschool.com>
2019-12-10 01:20:34 +0000

    [add] adding solution for 0x00-10 	new file:   10-check_cycle.c 	new file:   lists.h

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ *
+ * @head: head of the list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *head)
+{
+	listint_t *tmp1, *tmp2;
+
+	tmp1 = tmp2 = NULL;
+
+	if (head == NULL)
+		return (0);
+
+	tmp1 = tmp2 = head;
+
+	while (tmp1 != NULL && tmp2 != NULL)
+	{
+		tmp1 = tmp1->next;
+		tmp2 = tmp2->next->next;
+		if (tmp1 == tmp2)
+			return (1);
+		if (tmp2->next->next == NULL)
+			break;
+	}
+
+	return (0);
+}
