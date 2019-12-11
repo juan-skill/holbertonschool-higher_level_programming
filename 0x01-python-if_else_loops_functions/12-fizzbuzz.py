e89a0457e76b7d7b4531c5017d4cde4ff894913f
juan-skill <1294@holbertonschool.com>
2019-12-11 01:32:37 +0000

    [Add] adding solution for 0x012 	new file:   12-fizzbuzz.py

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+def fizzbuzz():
+    i = 1
+    while(i < 101):
+        if (i % 15 == 0):
+            print("FizzBuzz", end=' ')
+        elif (i % 3 == 0):
+            print("Fizz", end=' ')
+        elif (i % 5 == 0):
+            print("Buzz", end=' ')
+        else:
+            print("{:d}".format(i), end=' ')
+        i += 1
