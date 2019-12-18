de78f0aa69fe15770f9dac999a66675c15b779c6
juan-skill <1294@holbertonschool.com>
2019-12-18 03:49:06 +0000

    [Add] adding solution for 0x03-10 	new file:   10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def divisible_by_2(my_list=[]):
+
+    n_list = []
+
+    for i in my_list:
+
+        if i % 2:
+            n_list.append(False)
+        else:
+            n_list.append(True)
+
+    return (n_list)
