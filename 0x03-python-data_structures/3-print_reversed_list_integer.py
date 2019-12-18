2eebac9fd945be357b0fcbaeeb7d15cdcf23c4d2
juan-skill <1294@holbertonschool.com>
2019-12-18 01:57:05 +0000

    [Add] adding solution for 0x03-3 	new file:   3-print_reversed_list_integer.py

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,11 @@
+#!/bin/usr/python3
+
+
+def print_reversed_list_integer(list=[]):
+
+    if list is not None and len(list) > 0:
+        j = len(list) - 1
+
+        for item in list:
+            print("{:d}".format(list[j]))
+            j -= 1
