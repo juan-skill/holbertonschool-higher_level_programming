eff4fccd9902da081f6eb9a1cb42cc817a967745
juan-skill <1294@holbertonschool.com>
2019-12-20 01:06:10 +0000

    [Add] modify return 	modified:   12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,42 +6,42 @@ def roman_to_int(roman_string):
     number = 0
     str_roman = roman_string + ' '
 
-    if not roman_string or type(roman_string) != str:
-        return 0
+    if type(roman_string) is str or not roman_string:
 
-    i = 0
-    while (i < len(str_roman)):
+        i = 0
+        while (i < len(str_roman)):
 
-        if str_roman[i] == 'I':
-            if str_roman[i + 1] == 'V' or str_roman[i + 1] == 'X':
-                number -= 1
-            else:
-                number += 1
+            if str_roman[i] == 'I':
+                if str_roman[i + 1] == 'V' or str_roman[i + 1] == 'X':
+                    number -= 1
+                else:
+                    number += 1
 
-        elif str_roman[i] == 'V':
-            number += 5
+            elif str_roman[i] == 'V':
+                    number += 5
 
-        elif str_roman[i] == 'X':
-            if str_roman[i + 1] == 'L' or str_roman[i + 1] == 'C':
-                number -= 10
-            else:
-                number += 10
+            elif str_roman[i] == 'X':
+                if str_roman[i + 1] == 'L' or str_roman[i + 1] == 'C':
+                    number -= 10
+                else:
+                    number += 10
 
-        elif str_roman[i] == 'L':
-            number += 50
+            elif str_roman[i] == 'L':
+                number += 50
 
-        elif str_roman[i] == 'C':
-            if str_roman[i + 1] == 'D' or str_roman[i + 1] == 'M':
-                number -= 100
-            else:
-                number += 100
+            elif str_roman[i] == 'C':
+                if str_roman[i + 1] == 'D' or str_roman[i + 1] == 'M':
+                    number -= 100
+                else:
+                    number += 100
 
-        elif str_roman[i] == 'D':
-            number += 500
+            elif str_roman[i] == 'D':
+                number += 500
 
-        elif str_roman[i] == 'M':
-            number += 1000
+            elif str_roman[i] == 'M':
+                number += 1000
 
-        i += 1
+            i += 1
+        return number
 
-    return number
+    return 0
