454a79545217c6828d09ec7d877d49ef3ba26b38
juan-skill <1294@holbertonschool.com>
2019-12-20 00:51:44 +0000

    [Add] adding solutin for 0x04-12 	new file:   12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+
+def roman_to_int(roman_string):
+
+    number = 0
+    str_roman = roman_string + ' '
+
+    if not roman_string or type(roman_string) != str:
+        return 0
+
+    i = 0
+    while (i < len(str_roman)):
+
+        if str_roman[i] == 'I':
+            if str_roman[i + 1] == 'V' or str_roman[i + 1] == 'X':
+                number -= 1
+            else:
+                number += 1
+
+        elif str_roman[i] == 'V':
+            number += 5
+
+        elif str_roman[i] == 'X':
+            if str_roman[i + 1] == 'L' or str_roman[i + 1] == 'C':
+                number -= 10
+            else:
+                number += 10
+
+        elif str_roman[i] == 'L':
+            number += 50
+
+        elif str_roman[i] == 'C':
+            if str_roman[i + 1] == 'D' or str_roman[i + 1] == 'M':
+                number -= 100
+            else:
+                number += 100
+
+        elif str_roman[i] == 'D':
+            number += 500
+
+        elif str_roman[i] == 'M':
+            number += 1000
+
+        i += 1
+
+    return number
