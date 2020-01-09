36e20046572d3d8f8099511b1d6fea54de052a49
juan-skill <1294@holbertonschool.com>
2020-01-09 03:00:01 +0000

    [Add] Adding solution for 0x05-3 	new file:   3-safe_print_division.py

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def safe_print_division(a, b):
+
+    c = 0
+    try:
+        c = a / b
+    except (TypeError, ZeroDivisionError):
+        return None
+    finally:
+        print('Inside result: {}'.format(c))
+
+    return c
