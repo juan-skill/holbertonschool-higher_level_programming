e94df36245356cfcda3b6517d9beff38828301b1
juan-skill <1294@holbertonschool.com>
2020-01-10 01:17:42 +0000

    [Add] adding solution for 0x06-1 	new file:   1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """class square
+    Args:
+        size (int): size of the square.
+    Attributes:
+        __size (int): size of square
+
+    """
+
+    def __init__(self, size):
+        """Instantiation
+        Args:
+            size (int): size of the square.
+        Attributes:
+            __size (int): size of square
+
+        """
+
+        self.__size = size
