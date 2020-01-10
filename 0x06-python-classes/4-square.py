f55fbc51742258eaa313dc70f797f74a852b8233
juan-skill <1294@holbertonschool.com>
2020-01-10 04:00:36 +0000

    [Add] adding solution for 0x06-4 	new file:   4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+class Square:
+    """class square
+    Args:
+        size (int): size of the square.
+    Attributes:
+        __size (int): size of square
+
+    """
+
+    def __init__(self, size=0):
+        """"Initialize class
+        Args:
+            size (int): size of the square.
+        Attributes:
+            __size (int): size of square
+
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """size properties
+
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """size setter
+        Args:
+            value (int): size of the square.
+        Attributes:
+            __size (int): size of square
+
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """square the size
+        Returns:
+            Square of size.
+
+        """
+
+        return self.__size ** 2
