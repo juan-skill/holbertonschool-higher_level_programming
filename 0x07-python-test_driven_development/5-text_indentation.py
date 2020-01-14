f26f7111e49e285ceb7e4433a121f8602027e8b4
juan-skill <1294@holbertonschool.com>
2020-01-14 05:33:15 +0000

    [Add] adding solution for 0x07-5 	new file:   5-text_indentation.py 	new file:   tests/5-text_indentation.txt

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+   This module prints text with indentation
+   This module contains the following functions: text_indentation
+"""
+
+
+def text_indentation(text):
+    """
+       Prints a text with 2 new lines after these characters: <<.>> <<?>> <<:>>:
+    """
+    if text is None or not isinstance(text, str) or len(text) < 0:
+        raise TypeError("text must be a string")
+
+    text = text.replace('.', '.\n\n').replace('?', '?\n\n').replace(':', ':\n\n')
+
+    print("\n".join(list(l.strip() for l in text.split('\n'))), end="")
