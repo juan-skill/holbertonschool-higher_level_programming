487868be211e69fb37d74f3c14801518fa430ae9
juan-skill <1294@holbertonschool.com>
2020-01-14 14:58:22 +0000

    [Fix] adding test infinite number and nan 	modified:   0-add_integer.py 	modified:   tests/0-add_integer.txt

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,9 +10,9 @@ def add_integer(a, b=98):
     Adds two integers or floats as an Integer
     """
 
-    if type(a) is not int and type(a) is not float:
+    if a is None or (type(a) is not int and type(a) is not float):
         raise TypeError('a must be an integer')
-    if type(b) is not int and type(b) is not float:
+    if b is None or (type(b) is not int and type(b) is not float):
         raise TypeError('b must be an integer')
 
     return int(a) + int(b)
