af066ec6a0cdba031b59d752d09b40657f40dd7c
juan-skill <1294@holbertonschool.com>
2020-01-14 16:28:29 +0000

    [Fix] comparison each list of list 	modified:   2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -12,34 +12,36 @@ def matrix_divided(matrix, div):
     Returns new matrix.
     """
 
-    if matrix is None or \
+    new_matrix = []
+    if type(matrix) is not list or\
+       matrix is None or\
        len(matrix) is 0 or\
        len(matrix[0]) is 0:
-        raise TypeError('''matrix must be a matrix (list of lists)
-        of integers/floats''')
-
-    new_matrix = []
-    msg_type = "matrix must be a matrix (list of lists) of integers/floats"
+        raise TypeError('matrix must be a matrix (list of lists) of '
+                        'integers/floats')
 
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-    if div is 0:
+    elif div is 0:
         raise ZeroDivisionError("division by zero")
+
+    for row in matrix:
+        if len(row) != len(matrix[0]):
+            raise TypeError('Each row of the matrix must have the same size')
 
     try:
         for count, row in enumerate(matrix):
 
             if not isinstance(row, list):
-                raise TypeError(msg_type)
-
-            if len(row) != len(matrix[0]):
-                raise TypeError(msg_type)
+                raise TypeError('matrix must be a matrix (list of lists) of '
+                        'integers/floats')
 
             new_matrix.append(row[:])
             for val, item in enumerate(row):
 
                 if not isinstance(item, (int, float)):
-                    raise TypeError(msg_type)
+                    raise TypeError('matrix must be a matrix (list of lists) of'
+                        ' integers/floats')
                 new_matrix[count][val] = round(item / div, 2)
     except:
         raise
