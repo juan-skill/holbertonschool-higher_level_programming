3e9f48eed3f9f9bae6e143216a17b44172053a96
juan-skill <1294@holbertonschool.com>
2020-01-14 05:32:07 +0000

    [Add] adding solution for 0x07-3 	new file:   3-say_my_name.py 	new file:   tests/3-say_my_name.txt

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Say My Name - a function that says your name.
+This module contains the following functions: say_my_name
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Says your name.
+    first_name and last_name must be strings otherwise, will raise a TypeError
+    """
+
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+
+    print("My name is {:s} {:s}".format(first_name, last_name))
