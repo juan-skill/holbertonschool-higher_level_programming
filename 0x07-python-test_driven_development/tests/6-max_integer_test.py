92f0dd3b8cae649cd8b8fbced1409db6d0b286c7
juan-skill <1294@holbertonschool.com>
2020-01-14 19:04:11 +0000

    [Fix] adding cases: “max at the beginning”,  “max in the middle” 	modified:   tests/6-max_integer_test.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -14,11 +14,15 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([-2, -1, -4, -5, 1]), 1)
         self.assertEqual(max_integer([2.5, -1.2, 6.6, 9.9, 10.5]), 10.5)
         self.assertEqual(max_integer([98, 98, 98, 98]), 98)
+        self.assertEqual(max_integer([-1, -10, -3, -7]), -1)
         self.assertEqual(max_integer([10000]), 10000)
 
     def test_character(self):
         self.assertEqual(max_integer(["Hello", "World"]), "World")
         self.assertEqual(max_integer(['a', 'b', 'c']), 'c')
+
+    def test_type(self):
+        self.assertEqual(max_integer([3, -1, 98, float('nan')]), 98)
 
     def test_bool(self):
         self.assertEqual(max_integer([98, True, False, -9, 39, 1337]), 1337)
