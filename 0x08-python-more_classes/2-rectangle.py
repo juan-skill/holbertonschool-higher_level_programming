d1d5a819aa2de27d20037ce22833c81e82d3663b
juan-skill <1294@holbertonschool.com>
2020-01-15 04:43:28 +0000

    [Add] adding solution for 0x08-2 	new file:   2-rectangle.py

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""
+   This is the "Rectangle"  module
+   This module provides a Rectangle class
+"""
+
+
+class Rectangle:
+    """
+    The Rectangle Class:
+    Has two internal properties, and two methods to change them.
+    """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getter for width"""
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter for width"""
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for height"""
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for height"""
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """
+        Public instance method perimeter, returns perimeter of rectangle
+        """
+        return self.width * self.height
+
+    def perimeter(self):
+        """
+        Public instance method perimeter, returns perimeter of rectangle
+        """
+        if self.width == 0 or self.height == 0:
+            return 0
+        return self.width * 2 + self.height * 2
