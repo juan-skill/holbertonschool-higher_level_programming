9a338796b85da6d1c8b3803753ff6d012c738a89
juan-skill <1294@holbertonschool.com>
2020-01-15 04:42:02 +0000

    [Add] adding solution for 0x08-0 	new file:   0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+   This is the "Rectangle"  module
+   This module provides a Rectangle class
+"""
+
+
+class Rectangle:
+    """ C-lass Rectangle that defines a rectangle
+    """
+    pass
