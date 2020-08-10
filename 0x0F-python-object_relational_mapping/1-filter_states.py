748892c6de97c5dd79eaf47cd170c0251365bb30
juan-skill <1294@holbertonschool.com>
2020-08-10 03:31:59 +0000

    [add] Get all states: list all states from database hbtn_0e_0_usa 	new file:   1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+list all states with a name starting with N from the database hbtn_0e_0_usa
+ """
+
+from MySQLdb import connect
+from sys import argv
+
+
+def execute_query(cur):
+    """ Executing a sql query """
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
+    return cur.fetchall()
+
+
+def print_result(rows):
+    """ Printring the records """
+    for row in rows:
+        if row[1][0] == 'N':
+            print(row)
+
+
+if __name__ == '__main__':
+
+    # Connect to a MySQL database
+    db = connect(
+        user=argv[1], passwd=argv[2], db=argv[3], host="localhost", port=3306)
+
+    # Create a cursor handle
+    cur = db.cursor()
+
+    # Execute a sql query
+    rows = execute_query(cur)
+
+    # Prints the records
+    print_result(rows)
+
+    # Close all cursors
+    cur.close()
+
+    # Close all database
+    db.close()
