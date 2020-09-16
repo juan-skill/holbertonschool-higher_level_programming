986a8fe289a4e8db69e59e6072c1223c8e6b3c91
juan-skill <1294@holbertonschool.com>
2020-09-16 02:52:49 +0000

    [add] 6. Loop to languages: prints 3 lines: (like 1-multi_languages.js) new file:   6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+//  prints 3 lines: (like 1-multi_languages.js)
+
+const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
